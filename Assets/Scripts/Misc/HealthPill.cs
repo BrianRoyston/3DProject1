df76bb2edaf41eaae98d98a0503ce27945f316e2
Brian <brian@royston.com>
2020-02-03 23:16:08 -0800

    submission

---
+++ Assets/Scripts/Misc/HealthPill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPill : MonoBehaviour
+{
+    #region Editor Variabes
+    [SerializeField]
+    [Tooltip("The amount of health that this pill restores")]
+    private int m_HealthGain;
+    public int HealthGain
+    {
+        get
+        {
+            return m_HealthGain;
+        }
+    }
+
+    #endregion
+
+    #region
+
+    #endregion
+
+    #region
+
+    #endregion
+}
